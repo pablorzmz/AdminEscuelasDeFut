2585ca19cb961642ed4bb82cadd4ed28e0e8c2c0
Jose Pablo Ramirez Mendez <jose.ramirez16@ucr.ac.cr>
2018-07-16 01:08:33 -0600

    Cambios finales Trainings y Players. Todo suyo Gerald

---
+++ src/EscuelasFutbol/AdminEscuelasFut/Classes/C_Trainings.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminEscuelasFut
+{
+    class C_Trainings
+    {
+        private DataAccess dataAccess;
+        public C_Trainings()
+        {
+            dataAccess = new DataAccess();
+        }
+
+        public int deleteTraining(List<String> args)
+        {
+            List<DataAccess.storedProcData> procParams = new List<DataAccess.storedProcData>();
+            DataAccess.storedProcData parameter;
+
+            parameter.storedProcParam = "@fecha";
+            parameter.storedProcParamType = SqlDbType.Date;
+            parameter.userParams = args[0];
+            procParams.Add(parameter);
+
+            return dataAccess.executeStoreProcedure(procParams, "eliminarEntrenamiento"); ;
+        }
+        public int updateTraining(List<String> args)
+        {
+            List<DataAccess.storedProcData> procParams = new List<DataAccess.storedProcData>();
+            DataAccess.storedProcData parameter;
+
+            parameter = new DataAccess.storedProcData();
+            parameter.storedProcParam = "@fechaV";
+            parameter.storedProcParamType = SqlDbType.Date;
+            parameter.userParams = args[0];
+            procParams.Add(parameter);
+
+            parameter = new DataAccess.storedProcData();
+            parameter.storedProcParam = "@fechaN";
+            parameter.storedProcParamType = SqlDbType.Date;
+            parameter.userParams = args[1];
+            procParams.Add(parameter);
+
+            parameter = new DataAccess.storedProcData();
+            parameter.storedProcParam = "@horaN";
+            parameter.storedProcParamType = SqlDbType.Time;
+            parameter.userParams = args[2];
+            procParams.Add(parameter);
+
+            return dataAccess.executeStoreProcedure(procParams, "actualizarEntrenamiento");
+        }
+        public int insertNewTraining(List<String> args)
+        {
+            List<DataAccess.storedProcData> procParams = new List<DataAccess.storedProcData>();
+            DataAccess.storedProcData parameter;
+
+            parameter = new DataAccess.storedProcData();
+            parameter.storedProcParam = "@fecha";
+            parameter.storedProcParamType = SqlDbType.Date;
+            parameter.userParams = args[0];
+            procParams.Add(parameter);
+
+            parameter = new DataAccess.storedProcData();
+            parameter.storedProcParam = "@hora";
+            parameter.storedProcParamType = SqlDbType.Time;
+            parameter.userParams = args[1];
+            procParams.Add(parameter);
+
+            return dataAccess.executeStoreProcedure(procParams, "registrarEntrenamiento");
+        }
+        public void filldataGridViewTrainings(DataGridView dtgvTrainings, List<String> parameters)
+        {
+            const String defaultQuery = "SELECT * FROM Entrenamiento";
+            String filterQuery = "";
+            DataTable data;
+
+            if (parameters != null)
+            {
+                filterQuery =
+                    "SELECT " +
+                "      * " +
+                "FROM " +
+                "      Entrenamiento " +
+                "WHERE " +
+                "      Fecha like '%" + parameters[0] + "%' OR " +
+                "      Hora like '%" + parameters[0] + "%'";
+            }
+            if (filterQuery == "")
+            {
+                data = dataAccess.getTableFromQuery(defaultQuery);
+            }
+            else
+            {
+                data = dataAccess.getTableFromQuery(filterQuery);
+            }
+
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = data;
+
+            dtgvTrainings.DataSource = bindingSource;
+            dtgvTrainings.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dtgvTrainings.ReadOnly = true;
+            dtgvTrainings.AutoResizeColumns();
+            dtgvTrainings.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+}
--- src/EscuelasFutbol/AdminEscuelasFut/Forms/Trainings.cs
+++ src/EscuelasFutbol/AdminEscuelasFut/Forms/Trainings.cs
@@ -12,14 +12,18 @@ namespace AdminEscuelasFut
 {
     public partial class Trainings : Form
     {
+        private C_Trainings trainingsController;
+        private DataGridViewRow currentRow;
         public Trainings()
         {
             InitializeComponent();
+            trainingsController = new C_Trainings();
+            currentRow = null;
         }
 
         private void Trainings_Load(object sender, EventArgs e)
         {
-
+            trainingsController.filldataGridViewTrainings(this.dgvTrainingM,null);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -30,6 +34,113 @@ namespace AdminEscuelasFut
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void dgvTrainingM_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dgvTrainingM.RowCount)
+            {
+                currentRow = dgvTrainingM.Rows[e.RowIndex];
+                List<String> buffer = new List<string>();
+                Utilities.readCurrentRowFromDataGridView(dgvTrainingM, e.RowIndex, dgvTrainingM.ColumnCount, buffer);
+                this.dtpDateTrainingM.Value = Convert.ToDateTime(buffer[0]);
+                this.txtHourTrainingM.Text = buffer[1];
+            }            
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+
+            List<String> args = new List<string>();
+            args.Add(dtpDateTrainingM.Value.ToString("yyyy-MM-dd"));
+            args.Add(txtHourTrainingM.Text);
+            if (args[1] == "  :  :")
+            {
+                trainingsController.filldataGridViewTrainings(dgvTrainingM, null);
+            }
+            else
+            {
+                trainingsController.filldataGridViewTrainings(dgvTrainingM, args);
+            }
+            
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            TimeSpan time;          
+            if (txtHourTrainingM.Text == "  :  :" || txtHourTrainingM.Text.Contains(" ") || !TimeSpan.TryParse(txtHourTrainingM.Text, out time))
+            {
+                MessageBox.Show("Debe elegir una hora válida para el entrenamiento");
+                txtHourTrainingM.Focus();
+            }
+            else if (currentRow == null)
+            {
+                MessageBox.Show("Debe elegir un campos para actualizar desde la tabla de entrenamientos");
+            }
+            else
+            {
+                List<String> args = new List<string>();
+                args.Add(currentRow.Cells["Fecha"].Value.ToString());
+                args.Add(dtpDateTrainingM.Value.ToString("yyyy-MM-dd"));
+                args.Add(txtHourTrainingM.Text);
+                int result = trainingsController.updateTraining(args);
+                if (result == 0)
+                {
+                    MessageBox.Show("Entrenamiento actualizado con éxito");
+                    this.txtHourTrainingM.Text = "  :  :";
+                    trainingsController.filldataGridViewTrainings(dgvTrainingM, null);
+                }
+                else
+                {
+                    MessageBox.Show("Error al actualizar entrenamiento");
+                }
+            }
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            TimeSpan time;
+            if (txtHourTrainingM.Text == "  :  :" || txtHourTrainingM.Text.Contains(" ") || !TimeSpan.TryParse(txtHourTrainingM.Text, out time))
+            {
+                MessageBox.Show("Debe elegir una hora válida para el entrenamiento");
+                txtHourTrainingM.Focus();
+            }
+            else
+            {
+                List<String> args = new List<string>();
+                args.Add(dtpDateTrainingM.Value.ToString("yyyy-MM-dd"));
+                args.Add(txtHourTrainingM.Text);
+                int result = trainingsController.insertNewTraining(args);
+                if (result == 0)
+                {
+                    MessageBox.Show("Entrenamiento registrado con éxito");
+                    this.txtHourTrainingM.Text = "  :  :";
+                    trainingsController.filldataGridViewTrainings(dgvTrainingM, null);
+                }
+                else
+                {
+                    MessageBox.Show("Error al registrar entrenamiento");
+                }
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            List<String> args = new List<string>();
+            args.Add(dtpDateTrainingM.Value.ToString("yyyy-MM-dd"));
+
+            int result = trainingsController.deleteTraining(args);
+
+            if (result == 0)
+            {
+                MessageBox.Show("Entrenamiento borrado con éxito");
+                this.txtHourTrainingM.Text = "  :  :";
+                trainingsController.filldataGridViewTrainings(dgvTrainingM, null);
+            }
+            else
+            {
+                MessageBox.Show("Error al borrar entrenamiento");
+            }
         }
     }
 }
