89a255bd0971677ba0b7bf27efadfb1fa2a75a5e
Walter Bonilla Gutierrez <32148947+walterbg7@users.noreply.github.com>
2018-07-16 23:43:58 -0600

    Procedimientos y Vistas

--- src/StoredProcs.sql
+++ src/StoredProcs.sql
@@ -1,11 +1,7 @@
 ﻿USE DB_ERROR404
-/* Se necesitan al menos los siguientes procedimientos almacenados */
-
-
 /* Para agregar datos. */
 
 1- /* Registrar un nuevo pago mensual, de matricula o ambos de un jugador específico */
-
 GO
 --@tipo variable para saber que tipo de pago es, existen los siguientes casos:
 --0->Pago matricula
@@ -74,7 +70,6 @@ AS
 	/*Se inserta tupla Esta_en*/
 	INSERT INTO Esta_En VALUES(@cedulaJ, @escuela, '1');
 
-	
 GO
 
 3- /* Registrar un nuevo nivel que se imparte en las escuelas  */
@@ -99,25 +94,49 @@ CREATE PROCEDURE Registar_EntrenamientoJug
 	AS
 		INSERT INTO Asiste VALUES (@Cedula, @Fecha);
 GO
+
 6- /* Registrar una nueva instalación */
 GO
 CREATE PROCEDURE Registrar_Instalacion
 	@Direccion varchar(30), 
 	@Telefono varchar (8)  
 	AS
-		INSERT INTO Instalacion VALUES (@Direccion, @Telefono);
+		IF @Telefono = '' BEGIN
+			INSERT INTO Instalacion(Direccion) VALUES (@Direccion);
+		END
+
+		IF @Telefono <> '' BEGIN
+			INSERT INTO Instalacion VALUES (@Direccion, @Telefono);
+		END
 GO
 
 7- /* Registrar una nueva escuela */
 GO
-CREATE PROCEDURE Registrar_Escuela 
+CREATE PROCEDURE Registrar_Escuela
 	@Nombre varchar(30),
 	@Lugar varchar (30),
-	@Direccion varchar (30)
+	@Direccion varchar (30),
+	@tel1 VARCHAR(8), 
+	@tel2 VARCHAR(8)
 	AS
-		INSERT INTO Escuela VALUES (@Nombre, @Lugar, @Direccion);
+		IF '' <> @Direccion BEGIN
+			INSERT INTO Escuela VALUES (@Nombre, @Lugar, @Direccion);
+		END
+
+		IF '' = @Direccion BEGIN
+			INSERT INTO Escuela VALUES (@Nombre, @Lugar, NULL);
+		END
+
+		IF '' <> @tel1 BEGIN
+			INSERT INTO TelefonoE VALUES( @Nombre , @tel1 );
+		END
+
+		IF '' <> @tel2 BEGIN
+			INSERT INTO TelefonoE VALUES( @Nombre , @tel2 );
+		END
 GO
 
+8- /* Registrar un pago de mensualidad */
 GO
 CREATE PROCEDURE InsertarPagoMes
 		@escuela varchar(30),
@@ -127,6 +146,348 @@ CREATE PROCEDURE InsertarPagoMes
 		AS
 			INSERT INTO Mes VALUES (@escuela, @numRecibo, @mes, @ano);
 GO
+
+9- /* Agregarle numeros de telefonos a jugadores existentes */
+GO
+CREATE PROCEDURE agregarTelefonoJugadorExistente
+	@CedJugador char (9),
+	@Telefono1 varchar(8),
+	@Telefono2 varchar(8)
+	AS
+	IF @Telefono1 <> '-1' BEGIN
+		INSERT INTO 
+			TelefonoP
+		VALUES ( @CedJugador, @Telefono1 )
+	END
+	IF @Telefono2 <> '-1' BEGIN
+		INSERT INTO 
+			TelefonoP
+		VALUES ( @CedJugador, @Telefono2 )
+	END
+GO
+
+10- /* Insertar un encargado */
+GO
+CREATE  PROCEDURE insertarEncargado
+	@Cedula char(9),
+	@Nombre varchar(30),
+	@Apellido1 varchar(15),
+	@Apellido2 varchar (15)
+	AS
+	INSERT INTO PERSONA 
+	VALUES (@Cedula,@Nombre,@Apellido1,@Apellido2)
+
+	INSERT INTO Encargado
+		VALUES (@Cedula)
+GO
+
+11- /* Agregar a una escuela un nivel */
+GO
+CREATE PROCEDURE insertarNivelesAEscuela
+	@nombreEsc VARCHAR(30),
+	@numNivel TINYINT
+	AS BEGIN
+		INSERT INTO TIENE VALUES
+		(@nombreEsc, @numNivel);
+	END
+GO
+
+12- /* Insertar Telefonos a una Escuela */
+GO
+CREATE PROCEDURE insertarTelefonoE
+	@nombreEsc VARCHAR(30),
+	@tel1 VARCHAR(8), 
+	@tel2 VARCHAR(8)
+	AS BEGIN
+		IF '' <> @tel1 BEGIN
+			INSERT INTO TelefonoE VALUES( @nombreEsc , @tel1 );
+		END
+
+		IF '' <> @tel2 BEGIN
+			INSERT INTO TelefonoE VALUES( @nombreEsc , @tel2 );
+		END
+	END
+GO
+
+/* Para actualizar Datos */
+
+1- /* Actualizar o Insertar el Telefono de un Jugador */
+GO
+CREATE PROCEDURE actualizaOInsertarTelefonoNuevo
+	@Cedula CHAR(9),
+	@NTel1 VARCHAR(8),
+	@Ntel2 VARCHAR(8),
+	@VTel1 VARCHAR(8),
+	@Vtel2 VARCHAR(8)
+	AS
+	DECLARE @COUNT TINYINT
+
+	/*Existencia telefono 1*/
+	SELECT 
+		@COUNT = COUNT(*)
+	FROM	
+		TelefonoP
+	WHERE
+		TelefonoP.Cedula = @Cedula AND
+		TelefonoP.Telefono = @VTel1
+	IF @COUNT = 0 BEGIN
+		IF @VTel1 <> '' BEGIN
+			INSERT INTO
+				TelefonoP
+			VALUES (@Cedula,@VTel1)
+			PRINT 'TELEFONO 1 Insertado'
+		END
+	END
+	ELSE BEGIN
+		UPDATE 
+			TelefonoP
+		SET 
+			Telefono = @NTel1
+		WHERE
+			Cedula = @Cedula AND
+			Telefono = @VTel1
+			PRINT 'TELEFONO 1 ACTUALIZADO'
+	END
+		/*Existencia telefono 2*/
+	SELECT 
+		@COUNT = COUNT(*)
+	FROM	
+		TelefonoP
+	WHERE
+		TelefonoP.Cedula = @Cedula AND
+		TelefonoP.Telefono = @VTel2
+	IF @COUNT = 0 BEGIN
+		IF @VTel2 <> '' BEGIN
+			INSERT INTO
+				TelefonoP
+			VALUES (@Cedula,@VTel2)
+			PRINT 'TELEFONO 2 Insertado'
+		END
+	END
+	ELSE BEGIN
+		UPDATE 
+			TelefonoP
+		SET 
+			Telefono = @NTel2
+		WHERE
+			Cedula = @Cedula AND
+			Telefono = @VTel2
+		PRINT 'TELEFONO 2 ACTUALIZADO'
+	END
+GO
+
+2- /* Actualizar un Encargado */
+GO
+CREATE PROCEDURE actualizarEncargado
+	@NCedula char(9),
+	@NNombre varchar(30),	
+	@NApellido1 varchar(15),
+	@NApellido2 varchar (15),
+	@VCedula char(9),
+	@VNombre varchar(30),
+	@VApellido1 varchar(15),
+	@VApellido2 varchar (15)
+	AS
+	UPDATE
+		Persona
+	SET
+		Cedula = @NCedula,
+		NombreP = @NNombre,
+		Apellido1 = @NApellido1,
+		Apellido2 = @NApellido2
+	WHERE
+		Cedula = @VCedula AND
+		NombreP = @VNombre AND
+		Apellido1 = @VApellido1 AND
+		Apellido2 = @VApellido2
+GO
+
+3- /* Actualizar un Entrenamiento */
+GO
+CREATE PROCEDURE actualizarEntrenamiento
+	@fechaV DATE, 
+	@fechaN DATE, 
+	@horaN TIME(7)
+	AS 
+		UPDATE Entrenamiento 
+		SET Fecha = @fechaN,Hora = @horaN
+		WHERE Fecha = @fechaV
+GO
+
+4- /* Actualizar el entrenamiento de un Jugador */
+GO
+CREATE PROCEDURE actualizarEntrenamientoJugador
+	@CedJugador char(9),
+	@NuevoFecha date,
+	@ViejaFecha date
+	AS
+	UPDATE 
+		Asiste
+	SET 
+		Asiste.FecEntrenamiento = @NuevoFecha
+	WHERE
+		CedJugador = @CedJugador AND
+		FecEntrenamiento = @ViejaFecha;
+GO
+
+5- /* Actualizar una Escuela */
+GO
+CREATE PROCEDURE ActualizarEscuela
+	@nombreEscViejo VARCHAR(30),
+	@tel1Viejo VARCHAR(8),
+	@tel2Viejo VARCHAR(8),
+	@nombreEscNuevo VARCHAR(30),
+	@Lugar VARCHAR (30),
+	@Direccion VARCHAR (30),
+	@tel1Nuevo VARCHAR(8),
+	@tel2Nuevo VARCHAR(8)
+	AS BEGIN
+		IF @nombreEscViejo <> @nombreEscNuevo BEGIN
+			UPDATE Escuela 
+			SET Nombre = @nombreEscNuevo
+			WHERE Nombre = @nombreEscViejo;
+		END
+
+		UPDATE Escuela 
+		SET Lugar = @Lugar, DireccionInst = @Direccion
+		WHERE Nombre = @nombreEscNuevo;
+
+		IF @tel1Viejo <> '' BEGIN
+			UPDATE TelefonoE
+			SET Telefono = @tel1Nuevo
+			WHERE NombreEsc = @nombreEscNuevo AND Telefono = @tel1Viejo;
+		END
+
+		IF @tel1Viejo = '' AND @tel1Nuevo <> '' BEGIN
+			INSERT INTO TelefonoE VALUES( @nombreEscNuevo , @tel1Nuevo );
+		END
+
+		IF @tel2Viejo <> '' BEGIN
+			UPDATE TelefonoE
+			SET Telefono = @tel2Nuevo
+			WHERE NombreEsc = @nombreEscNuevo AND Telefono = @tel2Viejo;
+		END
+
+		IF @tel2Viejo = '' AND @tel2Nuevo <> '' BEGIN
+			INSERT INTO TelefonoE VALUES( @nombreEscNuevo , @tel2Nuevo );
+		END
+	END
+GO
+
+6- /* Actualizar una Instalacion */
+GO
+CREATE PROCEDURE actualizarInstalacion
+	@direccionV VARCHAR(30), 
+	@direccionN VARCHAR(30), 
+	@telefono VARCHAR(8)
+AS 
+	UPDATE Instalacion 
+	SET Direccion = @direccionN, Telefono = @telefono
+	WHERE Direccion = @direccionV
+GO
+
+7- /* Actualizar un Jugador */
+GO
+CREATE PROCEDURE actualizarJugador
+	@ViejaCedJugador	CHAR (9), 
+	@ViejoTelefono1Jugador	VARCHAR(8),
+	@ViejoTelefono2Jugador	VARCHAR(8),
+	@NuevaCedJugador	CHAR (9),
+	@Edad				TINYINT,
+	@Sexo				CHAR(1) ,
+	@FechaNacimiento	DATE,
+	@NuevaCedEncargado	CHAR (9),
+	@FechaIngreso		DATE,
+	@NombreJug			VARCHAR(30),
+	@Apellido1Jug		VARCHAR(15),
+	@Apellido2Jug		VARCHAR(15),
+	@NombreEncargado	VARCHAR(30),
+	@Apellido1Encar		VARCHAR(15),
+	@Apellido2Encar		VARCHAR(15),
+	@NomEscuela			VARCHAR(30),
+	@Nivel				TINYINT,
+	@NuevoTelefono1Jugador	VARCHAR(8),
+	@NuevoTelefono2Jugador	VARCHAR(8)
+	AS
+	BEGIN TRANSACTION
+	/*Primero actualiza la referencia en persona*/
+		UPDATE Persona
+		SET 
+			Persona.Cedula = @NuevaCedJugador
+		WHERE 
+			Persona.Cedula = @ViejaCedJugador;
+
+		UPDATE Jugador
+		SET 
+			Jugador.CedJugador = @NuevaCedJugador
+		WHERE 
+			Jugador.CedJugador = @ViejaCedJugador;
+	COMMIT
+	/*Se actualiza la informacíon personal del Jugador */
+	UPDATE 
+		Persona
+	SET
+		Persona.NombreP = @NombreJug,
+		Persona.Apellido1 = @Apellido1Jug,
+		Persona.Apellido2 = @Apellido2Jug
+	WHERE
+		Persona.Cedula = @NuevaCedJugador;
+
+	Update 
+		Jugador
+	SET
+		Jugador.Edad = @Edad,
+		Jugador.Sexo = @Sexo,
+		Jugador.FechaIngreso = @FechaIngreso,
+		Jugador.FechNac = @FechaNacimiento,
+		Jugador.CedEncargado = @NuevaCedEncargado
+	WHERE
+		Jugador.CedJugador = @NuevaCedJugador;
+	/*Se actualiza toda de Esta_En correspondiente */
+	UPDATE 
+		Esta_En
+	SET
+		Esta_En.NomEscuela = @NomEscuela,
+		Esta_En.Nivel = @Nivel
+	WHERE
+		Esta_En.CedJugador = @NuevaCedJugador;
+	/*Se telefonos de persona-Jugador */	
+	EXEC actualizaOInsertarTelefonoNuevo 
+				@NuevaCedJugador,
+				@NuevoTelefono1Jugador,
+				@NuevoTelefono2Jugador,
+				@ViejoTelefono1Jugador,
+				@ViejoTelefono2Jugador
+GO
+
+
+8- /* Actualizar un Nivel */
+GO
+CREATE PROCEDURE actualizarNivel 
+	@numeroV TINYINT, 
+	@numeroN TINYINT, 
+	@edadInicio TINYINT, 
+	@edadFin TINYINT
+AS 
+	UPDATE Nivel 
+	SET Numero = @numeroN, EdadInicio = @edadInicio, EdadFin = @edadFin
+	WHERE Numero = @numeroV
+GO
+
+9- /* Actualizar el Nivel de una Escuela */
+GO
+CREATE PROCEDURE ActualizarNivelesDeEscuela
+	@numNivelViejo TINYINT,
+	@numNivelNuevo TINYINT,
+	@nombreEsc VARCHAR(30)
+	AS BEGIN
+		UPDATE TIENE 
+		SET NumNivel = @numNivelNuevo
+		WHERE NomEsc = @nombreEsc AND NumNivel = @numNivelViejo;
+	END
+GO
+
+
 /* Para el borrado de datos */
 
 1- /* Eliminar un jugador de la base de datos */
@@ -134,56 +495,243 @@ GO
 CREATE PROCEDURE Eliminar_Jugador
 	@Cedula char(9)
 	AS
-		DELETE FROM Asiste
-		WHERE CedJugador = @Cedula;  /* Se elimina el jugador de la tabla asiste*/
+		DECLARE @CedEncargado CHAR(9)
+		DECLARE @Detalle varchar (500)
+		/*Se actualiza el detalle de los pagos asociados al jugador*/
+		DECLARE  cursorPagos CURSOR FOR
+		(SELECT 
+			Detalle 
+		FROM 
+			PagosDeJugador 
+		WHERE
+			CedJugador = @Cedula);
 
-		DELETE FROM Esta_En
-		WHERE CedJugador = @Cedula;
-
+		OPEN cursorPagos
+		FETCH NEXT FROM cursorPagos into @Detalle
+		WHILE @@FETCH_STATUS = 0 BEGIN
+			UPDATE
+				PagosDeJugador
+			SET
+				Detalle = 
+				CONCAT(@Detalle,' (Pago del jugador eliminado con cédula',@Cedula,')')
+			WHERE
+				CedJugador = @Cedula
+			FETCH NEXT FROM cursorPagos into @Detalle
+		END
+		CLOSE cursorPagos
+		DEALLOCATE  cursorPagos
+		/*finalmente se elimina al jugador*/
 		DELETE FROM Jugador 
 		WHERE CedJugador = @Cedula;
 
+		/*finalmente se elimina persona asociada*/
 		DELETE FROM Persona
 		WHERE Cedula = @Cedula;
+
+		/*Y se elimina el encargado si se dispara el trigger correspondiente*/
+		SELECT 
+			@CedEncargado = CedEncargado
+		FROM
+			Jugador
+		WHERE
+			CedJugador = @Cedula
+
+		DELETE FROM Encargado WHERE CedEncargado = @CedEncargado
 GO
+
 2- /* Eliminar una escuela de la base de datos */
+GO
+CREATE PROCEDURE eliminarEscuela
+	@nombre VARCHAR(30)
+	AS BEGIN
+		DECLARE @ced CHAR(9)
+		DECLARE Cursor1 CURSOR
+		FOR SELECT CedJugador FROM Esta_En WHERE NomEscuela = @nombre;
+		OPEN Cursor1
+		FETCH NEXT FROM Cursor1 INTO @ced
+		WHILE @@fetch_status = 0 BEGIN
+			EXEC Eliminar_Jugador @ced;
+			FETCH NEXT FROM Cursor1 INTO @ced
+		END
+		CLOSE Cursor1
+		DEALLOCATE Cursor1
+		DELETE FROM Escuela
+		WHERE Nombre = @nombre;
+	END
+GO
 
 3- /* Eliminar una instalacion de la base de datos */
-
 GO
-CREATE PROCEDURE eliminarInstalacion @direccion VARCHAR(30)
+CREATE PROCEDURE eliminarInstalacion 
+	@direccion VARCHAR(30)
 AS 
-DELETE Instalacion WHERE Direccion = @direccion
+	DELETE Instalacion WHERE Direccion = @direccion
 GO
 
 4- /* Eliminar un nivel de la base de datos */
 GO
-CREATE PROCEDURE eliminarNivel @numero TINYINT
+CREATE PROCEDURE eliminarNivel 
+	@numero TINYINT
 AS 
-DELETE 
-FROM Nivel
-WHERE Numero = @numero
+	DELETE 
+	FROM Nivel
+	WHERE Numero = @numero
 GO
 
 5- /* Eliminar un entrenamiento de la base de datos */
 GO
-CREATE PROCEDURE eliminarEntrenamiento @fecha DATE
+CREATE PROCEDURE eliminarEntrenamiento 
+	@fecha DATE
 AS 
-DELETE Entrenamiento 
-WHERE fecha = @fecha 
+	DELETE Entrenamiento 
+	WHERE fecha = @fecha 
 GO
 
 6- /* Eliminar un entrenamiento de un jugador de la base de datos */
+GO
+CREATE PROCEDURE EliminarEntrenamientoDeJugador
+	@CedJugador char (9),
+	@FechEntren date 
+	AS
+	DELETE  FROM Asiste WHERE Asiste.CedJugador = @CedJugador 
+							  AND Asiste.FecEntrenamiento = @FechEntren									
+GO
+
+7- /* Eliminar un Encargado */
+GO
+CREATE PROCEDURE eliminarEncargado
+	@Cedula char(9)
+	AS	
+	DELETE FROM Encargado WHERE CedEncargado = @Cedula
+	--DELETE FROM Persona WHERE Cedula = @Cedula
+GO
+
+8- /* Eliminar el nivel de una escuela */
+GO
+CREATE PROCEDURE eliminarNivelesDeEscuela
+	@nombreEsc VARCHAR(30),
+	@numNivel TINYINT
+	AS BEGIN
+		DELETE FROM TIENE 
+		WHERE NomEsc = @nombreEsc AND NumNivel = @numNivel;
+	END
+GO
+
+9- /* Eliminar un pago general de un jugador */
+GO
+CREATE PROCEDURE EliminarPagoGeneralJugador
+	@NombreEscuela varchar (30),
+	@NumeroRecibo varchar (5)
+	AS
+			DELETE FROM PagosDeJugador WHERE 
+			PagosDeJugador.NombreEscuela = @NombreEscuela AND
+			PagosDeJugador.NumeroRecibo = @NumeroRecibo
+GO
+
+10- /* Eliminar un pago de matricula de un jugador */
+GO
+CREATE PROCEDURE EliminarPagoMatriculaJugador
+	@NombreEscuela varchar (30),
+	@NumeroRecibo varchar (5)
+	AS
+			DELETE FROM PagoMatricula WHERE 
+			PagoMatricula.NombreEscuela = @NombreEscuela AND
+			PagoMatricula.NumeroRecibo = @NumeroRecibo
+GO
+
+11- /* Eliminar un pago de mensualidad de un jugador */
+GO
+CREATE PROCEDURE EliminarPagoMensualidadJugador
+	@NombreEscuela varchar (30),
+	@NumeroRecibo varchar (5),
+	@Mes char (3),
+	@Año char (4)
+	AS
+		DELETE FROM Mes WHERE 
+			Mes.NombreEscuela = @NombreEscuela AND
+			Mes.NumeroRecibo = @NumeroRecibo AND
+			Mes.Mes = @Mes AND
+			Mes.Año = @Año
+
+		DELETE FROM PagoMensualidad WHERE 
+			PagoMensualidad.NombreEscuela = @NombreEscuela AND
+			PagoMensualidad.NumeroRecibo = @NumeroRecibo
+GO
 
 
-7- /* Eliminar un pago de mensualidad, matricula o ambos de un jugador */
-
-/* Prupuesta para triggers */
+/* Triggers */
 
 1- /* Eliminar un encargador hasta que el número de jugadores donde él sale a cargo sea de cero
 y que no sea un entrenador,, adminsitrativo, etc */
+GO
+CREATE TRIGGER EliminaEncargadoSinJugadores 
+ON Encargado
+	INSTEAD OF DELETE	
+	AS
+	DECLARE @COUNTER TINYINT
+	DECLARE @CedEncargado char(9)
+	
+	SELECT 
+		@CedEncargado = CedEncargado
+	FROM 
+		DELETED;
 
-2- /*A la hora de insertar un jugador en un nivel, verificar que la edad corresponda con el nivel*/
+	SELECT 
+		@COUNTER =  COUNT(J.CedJugador) 
+	FROM 
+		Encargado as E left join Jugador as J on E.CedEncargado = J.CedEncargado
+	WHERE E.CedEncargado = @CedEncargado;	
+	
+	IF @COUNTER = 0 BEGIN
+		DELETE 
+		FROM 
+			Encargado
+		WHERE
+			CedEncargado = @CedEncargado
+		DELETE FROM Persona WHERE Cedula = @CedEncargado
+	END	
+GO
+
+
+2- /*A la hora de insertar un jugador en un nivel, verificar que la edad corresponda con el nivel si no insertarlo en el que corresponda */
+GO
+CREATE TRIGGER insertarJugANivel
+	ON Esta_En
+	INSTEAD OF INSERT
+	AS BEGIN
+		DECLARE @edad TINYINT,
+				@nivel TINYINT,
+				@ced CHAR(9),
+				@escuela VARCHAR(30),
+				@nivelEsc TINYINT,
+				@edadI TINYINT,
+				@edadF TINYINT
+		SELECT @nivel = Nivel, @ced = CedJugador, @escuela = NomEscuela 
+		FROM INSERTED;
+		
+		SELECT @edad = Edad
+		FROM Jugador
+		WHERE CedJugador = @ced;
+		
+		DECLARE Cursor1 CURSOR LOCAL
+		FOR SELECT NumNivel, EdadInicio, EdadFin FROM Tiene JOIN Nivel
+		ON Nivel.Numero = Tiene.NumNivel WHERE NomEsc = @escuela;
+		
+		OPEN Cursor1
+		FETCH NEXT FROM Cursor1 INTO @nivelEsc, @edadI, @edadF;
+		WHILE @@fetch_status = 0 BEGIN
+			
+			IF @edad >= @edadI AND @edad <= @edadF BEGIN
+				INSERT INTO Esta_En VALUES
+				(@ced, @escuela, @nivelEsc);
+			END
+
+			FETCH NEXT FROM Cursor1 INTO @nivelEsc, @edadI, @edadF;
+		END
+		CLOSE Cursor1
+		DEALLOCATE Cursor1
+	END
+GO
 
 
 
--- src/Views.sql
+++ src/Views.sql
@@ -1,8 +1,72 @@
 USE DB_Error404
 
+/* Pagos de un jugador */
 GO
 CREATE VIEW VistaPagosJugador
 AS
 	SELECT NombreP as Nombre, CedJugador as [N° Cedula], NumeroRecibo as [N° Recibo], Fecha, Monto, Detalle, NombreEscuela as Escuela
-	FROM PagosDeJugador join Persona ON Cedula = CedJugador;
+	FROM PagosDeJugador left join Persona ON Cedula = CedJugador;
+GO
+
+/* Entrenamientos de un jugador */
+GO
+CREATE VIEW VerEntrenamientosJugador
+AS
+SELECT
+	Esta_En.NomEscuela as Escuela,
+	Asiste.CedJugador AS Cédula,
+	Persona.NombreP AS Nombre,
+	Persona.Apellido1 AS Apellido,
+	Asiste.FecEntrenamiento AS Fecha 
+FROM 
+	Asiste join Persona on Asiste.CedJugador = Persona.Cedula 
+	join Esta_en on Asiste.CedJugador = Esta_En. CedJugador
+GO
+
+/* Ver la informacion del encargado de un jugador */
+GO
+CREATE VIEW verInforEncargado
+AS
+SELECT 
+	Encargado.CedEncargado AS Cedula,
+	Persona.NombreP AS Nombre,
+	Persona.Apellido1,
+	Persona.Apellido2
+FROM 
+	Encargado join Persona on Encargado.CedEncargado = Persona.Cedula;
+GO
+
+/* Ver la informacion de las cedulas de los jugadores */
+GO
+CREATE VIEW VistaInfoCedulasJugador
+	AS
+		SELECT CedJugador as Cedula ,NombreP as Nombre, Apellido1 as [Primer Apellido], Apellido1 as [Segundo Apellido]
+		FROM Jugador join Persona ON Cedula = CedJugador;
+GO
+
+/* Ver la informacion de un jugador */
+GO
+CREATE VIEW VistaInformacionJugador
+AS
+SELECT 
+	Jugador.CedJugador as 'N° Cédula',
+	Persona.NombreP as 'Nombre',
+	Persona.Apellido1 as 'Primer Apellido',
+	Persona.Apellido2 as 'Segundo Apellido',
+	Jugador.Edad as 'Edad',
+	Jugador.Sexo  as 'Sexo',
+	Jugador.FechNac as 'Fecha Nacimiento',
+	Jugador.FechaIngreso as 'Fecha Ingreso',
+	Esta_En.NomEscuela as  'Escuela',
+	Esta_En.Nivel as 'Nivel',
+	t.CedEncargado as 'N° Cédula Encargado', 	
+	t.NombreP  as 'Nombre Encargado',
+	t.Apellido1 as 'Primer Apellido Encargado',
+	t.Apellido2 as 'Segundo Apellido Encargado'
+FROM 
+	Jugador inner join Persona on Persona.Cedula = Jugador.CedJugador
+	inner join (SELECT Encargado.CedEncargado,	Persona.NombreP, 
+	Persona.Apellido1,Persona.Apellido2 FROM Encargado inner join Persona
+	on Encargado.CedEncargado = Persona.Cedula) as t on Jugador.CedEncargado = t.CedEncargado
+	inner join Esta_en on Jugador.CedJugador = Esta_En.CedJugador;
 GO
